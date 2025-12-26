5fb38ccff701c4d9d51aeb7425da7d78eb42febd
s12219496-source <s12219496@stu.najah.edu>
2025-12-26 02:16:45 +0200

    fff

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM php:8.2-apache
-# تثبيت تعريفات قاعدة البيانات كما في شرح الحزم
+
 RUN docker-php-ext-install mysqli
-# نسخ ملفات مشروعك من المجلد الحالي إلى داخل الحاوية
+
 COPY . /var/www/html/
 EXPOSE 80
--- push_code.sh
+++ push_code.sh
@@ -1,17 +1,16 @@
-#!/bin/bash
 
-# الانتقال لمجلد المشروع
 cd ~/UnixProject
 
-# إضافة التعديلات
-git add .
 
-# طلب رسالة الـ Commit من المستخدم
+git add .
+git reset config.php
+
+
 echo "Enter commit message:"
 read message
 
-# عمل Commit و Push
+
 git commit -m "$message"
 git push origin main
 
-echo "Code uploaded successfully to GitHub!"
+echo "Code uploaded successfully to GitHub (Config skipped)!"
