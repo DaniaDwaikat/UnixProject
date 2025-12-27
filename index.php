38157cfe5d7e117069e2f8fe4402c51f9abec356
s12219496-source <s12219496@stu.najah.edu>
2025-12-27 16:14:10 +0200

    jana final test

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>سجل حضور وغياب الطلاب </title>
+<title>:سجل حضور وغياب الطلاب </title>
 <link rel="stylesheet" href="style.css">
 
 </head>
