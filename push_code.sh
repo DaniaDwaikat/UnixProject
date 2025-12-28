e005365fd42d43b3c954254417b4ffd237e2a221
s12219496-source <s12219496@stu.najah.edu>
2025-12-28 23:22:28 +0200

    j

--- push_code.sh
+++ push_code.sh
@@ -4,7 +4,8 @@ git add .
 
 echo "📝 Enter commit message:"
 read message
-if [ -z "$message" ]; then
+if [ -z "$message" ]; 
+then
     message="Update from script: $(date)"
 fi
 
@@ -12,14 +13,16 @@ git commit -m "$message"
 
 git pull --rebase origin main
 
-if [ $? -ne 0 ]; then
+if [ $? -ne 0 ];
+ then
     echo " Pull failed. Resolve conflicts manually."
     exit 1
 fi
 
 git push origin main
 
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 ];
+ then
     echo " Success! Jenkins will start the build now."
 else
     echo "Push failed."
